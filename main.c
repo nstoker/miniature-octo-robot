674f01a9d3f4246cf375a74ab924b085b6e1055f
Neil Stoker <neil.stoker@gmail.com>
2012-12-02 16:51:47 +0000

    Added LGPL license text to files

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
-// The functions contained in this file are pretty dummy
-// and are included only as a placeholder. Nevertheless,
-// they *will* get included in the static library if you
-// don't remove them :)
-// 
-// Obviously, you 'll have to write yourself the super-duper
-// functions to include in the resulting library...
-// Also, it's not necessary to write every function in this file.
-// Feel free to add more files in this project. They will be
-// included in the resulting library.
+/*     This file is part of lib65.
 
-// A function adding two integers and returning the result
+    lib65 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser Public License for more details.
+
+    You should have received a copy of the GNU Lesser Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 int SampleAddInt(int i1, int i2)
 {
     return i1 + i2;
@@ -25,6 +29,6 @@ void SampleFunction1()
 int SampleFunction2()
 {
     // insert code here
-    
+
     return 0;
 }
